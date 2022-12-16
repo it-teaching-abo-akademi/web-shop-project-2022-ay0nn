f73ffffc82c671295bd226140bde37f3599cad03
ay0nn <49310394+ay0nn@users.noreply.github.com>
2022-12-16 02:00:32 +0200

    header,footer done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header />
+    <main className='py-3'>
+      <Container>
+      <h1>Welcome</h1>
+      </Container>
+    </main>
+    <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <Container>
+            <Row>
+                <Col className='text-center py-3'>Copyright @Hedelmäkauppa
+                </Col>
+            </Row>
+        </Container>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
