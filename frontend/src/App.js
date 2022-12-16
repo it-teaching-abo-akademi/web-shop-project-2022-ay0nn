9305b7384d60e803e04e2fb0405dc8edd63a1493
ay0nn <49310394+ay0nn@users.noreply.github.com>
2022-12-16 03:41:02 +0200

    Card view done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Homepage from './screens/Homepage'
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
     <Header />
     <main className='py-3'>
       <Container>
-      <h1>Welcome</h1>
+      <Homepage />
       </Container>
     </main>
     <Footer />
