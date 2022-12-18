b215b9be6bd5c53386768b56bb966d8fcab9e5a8
ay0nn <49310394+ay0nn@users.noreply.github.com>
2022-12-18 04:03:09 +0200

    Django added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Homepage from './screens/Homepage'
+import Shop from './screens/Shop'
 
 function App() {
   return (
-    <div>
+    
+    <Router>
     <Header />
     <main className='py-3'>
       <Container>
-      <Homepage />
+        <Homepage/>
+       {/* <Route exact path='/' component={<Homepage />} />
+       <Route path='/product/:id' component={<Shop />} /> */}
       </Container>
     </main>
     <Footer />
-    </div>
+    </Router>
   );
 }
 
