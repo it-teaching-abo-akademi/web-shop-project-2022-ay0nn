00555dab937a1b429edd7a18cdb2f8444f90d0e1
ay0nn <49310394+ay0nn@users.noreply.github.com>
2022-12-19 13:16:49 +0200

    django added

---
+++ backend/base/products.py
@@ -0,0 +1,35 @@
+products = [
+  
+    {
+        '_id': "2",
+        'name':'Mango',
+        'image':'/images/mango.jpeg',
+        'description':'Ekdom fresh from Kola Bagan',
+        'price':6.8,
+        'inStock': 40,
+        'rating':4.2,
+        'reviews':24,
+    },
+    {
+        '_id': "3",
+        'name':'Grape',
+        'image':'/images/grape.jpeg',
+        'description':'Ekdom grape fresh from Kola Bagan',
+        'price':3.8,
+        'inStock': 15,
+        'rating':4.5,
+        'reviews':13,
+    },
+    {
+        '_id': "4",
+        'name':'Blue Berry',
+        'image':'/images/bberry.jpeg',
+        'description':'Ekdom Blueberry fresh from Kola Bagan',
+        'price':4.8,
+        'inStock':11,
+        'rating':4.9,
+        'reviews':25,
+    },
+
+]
+
